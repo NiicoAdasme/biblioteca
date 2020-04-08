4c63f31b2ff62f6806586ba4508a762b1e61a5cf
NiicoAdasme <nicolas.wladimir@gmail.com>
2020-04-08 02:46:44 -0400

    Pruebas con rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Annotation\Route as AnnotationRoute;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::get('/', function () { // ruteo con closure, esta funcion no lo guarda en caché (no recomendado)
+    return view('welcome'); 
+});*/
+
+Route::view('/', 'welcome');
+
+Route::get('permiso/{nombre?}/{edad?}', 'PermisoController@index')->where(['nombre'=>'[A-Za-z]+', 'edad' => '[0-9]+'])->name('permiso'); // ruteo desde controlador
+
+Route::get('admin/sistema/permiso', 'PermisoController@index')->name('permiso'); 
+/* Lo referencio como {{route('permiso')}} en la view
+Evito recordar la url que es admin/sistema/permiso {{url(''admin/sistema/permiso)}}*/
